9ef1d3a603edc462b5146f47a4c8ba778b3e4f1b
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-11-13 13:46:34 -0800

    Add project files.

---
+++ SolutionTimeLog/OpenTimeLog.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace SolutionTimeLog
+{
+    internal sealed class OpenTimeLog
+    {
+        public const int CommandId = 0x0100;
+
+        public static readonly Guid CommandSet = new Guid("0f4b81c2-20ef-458e-8038-e1eecb90a0ee");
+
+        private readonly AsyncPackage package;
+
+        private OpenTimeLog(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        public static OpenTimeLog Instance
+        {
+            get;
+            private set;
+        }
+
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new OpenTimeLog(package, commandService);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "OpenTimeLog";
+
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ SolutionTimeLog/SolutionTimeLogPackage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.Shell;
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+using Task = System.Threading.Tasks.Task;
+
+namespace SolutionTimeLog
+{
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[Guid(PackageGuids.TimeLogPackageString)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	public sealed class SolutionTimeLogPackage : AsyncPackage
+	{
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+		    await OpenTimeLog.InitializeAsync(this);
+		}
+	}
+}
